65816e09df50f2e09f4fc1f646808ac8c0d4c7d9
Nick Gundobin <ngundobin@gmail.com>
2020-12-15 17:36:13 -0500

    make chains is complete/made an attempt to complete makeText with some errors

--- markov.js
+++ markov.js
@@ -1,29 +1,73 @@
 /** Textual markov chain generator */
 
+class MarkovMachine { /** build markov machine; read in text.*/
 
-class MarkovMachine {
+    constructor(text) {
+        let words = text.split(/[ \r\n]+/);
+        this.words = words.filter(c => c !== "");
+        this.makeChains();
+    }
 
-  /** build markov machine; read in text.*/
-
-  constructor(text) {
-    let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
-    this.makeChains();
-  }
-
-  /** set markov chains:
+    /** set markov chains:
    *
    *  for text of "the cat in the hat", chains will be
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
-  makeChains() {
-    // TODO
-  }
+    makeChains() { // TODO
+        const obj = {};
+        for (let i = 0; i < this.words.length; i++) {
+            if (this.words[i + 1]) {
+                if (this.words[i] in obj) {
+                    obj[this.words[i]].push(this.words[i + 1]);
+                } else {
+                    const arr = [];
+                    arr.push(this.words[i + 1]);
+                    obj[this.words[i]] = arr;
+                }
+            } else {
+                if (this.words[i] in obj) {
+                    obj[this.words[i]].push(null);
+                } else {
+                    obj[this.words[i]] = [].push(null);
+                }
+            }
+        }
+        return obj;
+    }
 
 
-  /** return random text from chains */
+    /** return random text from chains */
 
-  makeText(numWords = 100) {
-    // TODO
-  }
+    makeText(numWords = 100) { // TODO
+        const obj = this.makeChains();
+        console.log(obj);
+        console.log(obj['Sam-I-am']);
+
+
+        const keysObj = Object.keys(obj);
+        console.table(keysObj)
+
+        const size = keysObj.length;
+        const genNum = Math.floor(Math.random() * (size + 1));
+        let startingVal = keysObj[genNum];
+        const res = [];
+        res.push(startingVal);
+
+        for (let i = 0; i < numWords; i++) {
+            console.log(startingVal);
+
+            const arr = obj[startingVal];
+            const randInd = Math.floor(Math.random() * (arr.length + 1));
+            startingVal = arr[randInd];
+            if (! startingVal) {
+                break;
+            }
+            res.push(startingVal);
+        }
+        return res.join(' ')
+    }
 }
+
+module.exports = {
+    MarkovMachine: MarkovMachine
+};
---
+++ rand.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const {MarkovMachine} = require('./markov');
+
+
+const content = fs.readFileSync("eggs.txt", 'utf8');
+
+const machine = new MarkovMachine(content);
+console.log(machine.words);
+console.log(machine.makeText());
+// console.log(content);
