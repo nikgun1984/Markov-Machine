787f1461a875ae3496b6d2fcdd3feff4e5f1b450
Nick Gundobin <ngundobin@gmail.com>
2020-12-15 20:28:31 -0500

    removed rand.js file

--- rand.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const {MarkovMachine} = require('./markov');
-
-
-const content = fs.readFileSync("eggs.txt", 'utf8');
-
-const machine = new MarkovMachine(content);
-console.log(machine.words);
-console.log(machine.makeText());
