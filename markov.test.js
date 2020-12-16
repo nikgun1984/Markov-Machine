ab2619f1207d3a65d201cbfd0205ae7678db175f
Nick Gundobin <ngundobin@gmail.com>
2020-12-15 21:01:05 -0500

    tests

---
+++ markov.test.js
@@ -0,0 +1,16 @@
+const {MarkovMachine} = require('./markov');
+
+describe("testing MarkovMachine class", function () {
+    let content;
+    let machine;
+    beforeEach(function () {
+        content = "the cat in the hat is in the hat";
+        machine = new MarkovMachine(content);
+    });
+
+    test("test makeChains method", function () {
+        expect(Object.keys(machine.makeChains())).toContain('cat');
+        expect(machine.makeChains()["the"].length).toEqual(3);
+
+    })
+})
